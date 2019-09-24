651d86ae6fa6b0bcbf2c22241df730801c9f0c29
alexe <cherkashin@ubrr.ru>
2019-09-24 18:20:36 +0500

    tuesday_gohome

--- test_main_page.py
+++ test_main_page.py
@@ -17,7 +17,9 @@ def test_guest_should_see_login_link(browser):
 
 
 def test_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url) # текущий браузер и текущая страница
+    login_page.should_be_login_page()
